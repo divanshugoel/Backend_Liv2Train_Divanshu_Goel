a3cf69c928f46d162e94b4d1f262549950304355
divanshugoel <divanshu.goel339@gmail.com>
2021-05-07 22:11:58 +0530

    project main directory added

---
+++ src/main/java/org/liv2train/app/student/training/centre/locator/StudentTrainingCentreLocatorApplication.java
@@ -0,0 +1,13 @@
+package org.liv2train.app.student.training.centre.locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrainingCentreLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrainingCentreLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/liv2train/app/studenttrainingcentrelocator/StudentTrainingCentreLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liv2train.app.studenttrainingcentrelocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrainingCentreLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
